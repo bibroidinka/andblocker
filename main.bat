cb2929dc77068adec212d5cb08a47ebb787e297a
bibroidinka <115043830+bibroidinka@users.noreply.github.com>
2025-09-27 19:01:29 +0200

    Update main.bat

--- main.bat
+++ main.bat
@@ -16,4 +16,20 @@ IF %ERRORLEVEL%==0 (
     ECHO Ошибка при блокировке. Убедитесь, что у вас есть права администратора.
 )
 
+REM Проверяем, существует ли ветка Policies\System
+REG QUERY "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO Создаем ветку Policies\System
+    REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /f
+)
+
+REM Устанавливаем параметр DisableRegistryTools
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f
+
+IF %ERRORLEVEL%==0 (
+    ECHO Редактор реестра успешно отключен.
+) ELSE (
+    ECHO Ошибка при отключении. Убедитесь, что у вас есть права администратора.
+)
+
 PAUSE
