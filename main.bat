5f0bf8127d441f75a910b4083fa0cbd0979dcacb
bibroidinka <115043830+bibroidinka@users.noreply.github.com>
2025-09-27 18:53:59 +0200

    Create main.bat

---
+++ main.bat
@@ -0,0 +1,19 @@
+@ECHO OFF
+
+REM 
+REG QUERY "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO Создаем ветку Policies\System
+    REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /f
+)
+
+REM 
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f
+
+IF %ERRORLEVEL%==0 (
+    ECHO Диспетчер задач успешно заблокирован.
+) ELSE (
+    ECHO Ошибка при блокировке. Убедитесь, что у вас есть права администратора.
+)
+
+PAUSE
